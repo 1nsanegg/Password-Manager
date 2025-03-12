7133b19d6538be1d992b18f138c8288aaf6d073b
1nsanegg <107063052+1nsanegg@users.noreply.github.com>
2025-03-12 22:03:00 +0700

    build UI

---
+++ main.py
@@ -0,0 +1,49 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20,pady=20)
+
+canvas = Canvas(width=200,height=200)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100,100,image=lock_img)
+canvas.grid(row=0,column=1)
+
+# Website
+website_text = Label(text="Website")
+website_text.grid(row=1, column=0)
+
+website_input = Entry()
+website_input.grid(row=1,column=1,columnspan=2,sticky="EW")
+
+# Email/Username
+email_user_text = Label(text="Email/Username:")
+email_user_text.grid(row=2, column=0)
+
+email_user_input = Entry()
+email_user_input.grid(row=2,column=1,columnspan=2,sticky="EW")
+
+# Password
+password_text = Label(text="Password:")
+password_text.grid(row=3, column=0)
+
+password_input = Entry()
+password_input.grid(row=3,column=1,sticky="EW")
+
+# Generate button
+generate_btn = Button(text="Generate Password")
+generate_btn.grid(row=3,column=2)
+
+# Add button
+add_btn = Button(text="Add", width=35)
+add_btn.grid(row=4, column=1,columnspan=2,sticky="EW")
+
+
+
+window.mainloop()
