6a8113afe1a259eaf2ff6cdfa92c271a27f819ad
1nsanegg <107063052+1nsanegg@users.noreply.github.com>
2025-03-20 21:21:55 +0700

    add generate password and noti dialog

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 from tkinter import *
-
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    # for char in range(nr_letters):
+    #   password_list.append(random.choice(letters))
+
+    rand_letter = [choice(letters) for _ in range(randint(8, 10))]
+    rand_symbol = [ choice(symbols) for _ in range(randint(2, 4))   ]
+    rand_number = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list =  rand_letter + rand_number + rand_symbol
+
+    shuffle(password_list)
+
+    password_gen = "".join(password_list)
+
+    password_input.delete(0, "end")
+    password_input.insert(0, password_gen)
+    pyperclip.copy(password_gen)
+    print(f"Your password is: {password_gen}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    with open("data.txt", "a") as f:
+    website = website_input.get()
+    email = email_user_input.get()
+    password = password_input.get()
 
-        website = website_input.get()
-        email = email_user_input.get()
-        password =  password_input.get()
-        f.write(f"{website}     |       {email}     |       {password}\n")
-    website_input.delete(0, "end")
-    email_user_input.delete(0, "end")
-    email_user_input.insert(0, "tuan1214502@gmail.com")
-    password_input.delete(0, "end")
-    website_input.focus()
+    if len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                      f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+
+            with open("data.txt", "a") as f:
+
+                f.write(f"{website}     |       {email}     |       {password}\n")
+            website_input.delete(0, "end")
+            email_user_input.delete(0, "end")
+            email_user_input.insert(0, "tuan1214502@gmail.com")
+            password_input.delete(0, "end")
+            website_input.focus()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -51,7 +84,7 @@ password_input = Entry()
 password_input.grid(row=3,column=1,sticky="EW")
 
 # Generate button
-generate_btn = Button(text="Generate Password")
+generate_btn = Button(text="Generate Password", command=generate_password)
 generate_btn.grid(row=3,column=2)
 
 # Add button
